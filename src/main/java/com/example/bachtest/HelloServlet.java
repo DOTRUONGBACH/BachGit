e0003860b428147ab38b2cd2aa006e5ce00cd496
ducanhgtcc <ducanhgt.nd@gmail.com>
2022-12-26 15:05:36 +0700

    ducanh

--- src/main/java/com/example/bachtest/HelloServlet.java
+++ src/main/java/com/example/bachtest/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello anh bách";
+        message = "Hello Duc Anh";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
---
+++ src/main/java/ducanh/ducanh.java
@@ -0,0 +1,9 @@
+package ducanh;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class ducanh extends HttpServlet {
+    private String product;
+}
